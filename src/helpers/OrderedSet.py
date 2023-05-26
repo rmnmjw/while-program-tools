cbc396ac6e1738b7dd8fac075b7b276a959562f7
Roman Majewski <rmnmjw@posteo.de>
2023-05-26 07:30:57 +0200

    Improve OrderedSet

--- src/helpers/OrderedSet.py
+++ src/helpers/OrderedSet.py
@@ -1,17 +1,35 @@
 import collections
 
 class OrderedSet(collections.OrderedDict):
-    def __init__(self):
+    def __init__(self, collection=None):
         super().__init__()
+        if collection != None:
+            self.update(collection)
     
     def __repr__(self):
         return '{' + str(list(self.keys()))[1:-1] + '}'
     
+    def __sort(self):
+        items = list(self)
+        items.sort()
+        for k in items:
+            del self[k]
+            self[k] = None
+    
     def add(self, val):
         self[val] = None
+        self.__sort()
     
     def update(self, vals):
         for v in vals:
-            self[v] = None
+            self.add(v)
+    
+    def union(self, other):
+        s = OrderedSet()
+        for v in self:
+            s.add(v)
+        for v in other:
+            s.add(v)
+        return s
     
     # TODO: add all the other needed methods
